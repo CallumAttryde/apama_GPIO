f313bec9fbdfdd465f752beb966805c85ddb21e6
CallumAttryde <CallumAttryde@gmail.com>
2018-03-12 16:07:48 +0000

    altered buttonpress test while investigating waitForSignal bug

--- tutorials/ButtonPress/run.py
+++ tutorials/ButtonPress/run.py
@@ -4,8 +4,8 @@ class PySysTest(GPIOBaseTest):
 
 	def execute(self):
 		self.start()
-		self.correlator.injectMonitorscript(filenames=['tutorial.mon'])		
-		self.waitForSignal('correlator.out', expr='.*Monitor Loaded.*')
+		self.correlator.injectMonitorscript(filenames=['tutorial.mon'])
+		self.wait(4.0) # wait for monitor to load - currently investigating a strange corruption issue when using waitForSignal
 		self.writePin(22, 1)
 		self.waitForSignal('correlator.out', expr='Button Pressed')
 
