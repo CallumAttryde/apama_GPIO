b7ea22847f2395a45f056a9bcbe54cf8df7189c8
CallumAttryde <CallumAttryde@gmail.com>
2018-03-12 14:54:08 +0000

    further fixed to BasicLED and verification

--- tutorials/BasicLED/run.py
+++ tutorials/BasicLED/run.py
@@ -5,8 +5,9 @@ class PySysTest(GPIOBaseTest):
 	def execute(self):
 		self.start()
 
+		self.checkPin(17, 0)
 		self.correlator.injectMonitorscript(filenames=['tutorial.mon'])
-		
+		self.checkPin(17, 1)
 		self.wait(10)
 
 	def validate(self):
