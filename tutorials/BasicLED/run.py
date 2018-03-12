27d12ff7f18afa89a0d929a9d4ddff2664b87e10
CallumAttryde <CallumAttryde@gmail.com>
2018-03-12 17:48:49 +0000

    improved BasicLED test, now a good pattern for future tests

--- tutorials/BasicLED/run.py
+++ tutorials/BasicLED/run.py
@@ -4,11 +4,14 @@ class PySysTest(GPIOBaseTest):
 
 	def execute(self):
 		self.start()
+		self.initialPinSetup(lowPins=[17])
 
-		self.checkPin(17, 0)
 		self.correlator.injectMonitorscript(filenames=['tutorial.mon'])
+		self.waitForSignal('correlator.out', expr='LED on', errorExpr=['TEST FAILED'], timeout=10)
 		self.checkPin(17, 1)
-		self.wait(10)
+		self.correlator.sendEventStrings('Step(1)')
+		self.waitForSignal('correlator.out', expr='LED off', errorExpr=['TEST FAILED'], timeout=10)
+		self.checkPin(17, 0)
 
 	def validate(self):
 		pass
--- tutorials/test/GPIO/BaseTest.py
+++ tutorials/test/GPIO/BaseTest.py
@@ -15,10 +15,21 @@ class GPIOBaseTest(BaseTest):
 
 		os.environ["LD_LIBRARY_PATH"] = self.BASE_DIR + os.pathsep + os.environ["LD_LIBRARY_PATH"]
 
+	def __del__(self):
+		GPIO.cleanup()
+
 	def start(self):
 		self.correlator = CorrelatorHelper(self)#, name='GPIOCorrelator')
 		self.correlator.start()
 		self.correlator.injectMonitorscript(filenames=[os.path.join(self.BASE_DIR, 'GPIOPlugin.mon')])
+
+	def initialPinSetup(self, lowPins=None, highPins=None):
+		if (lowPins):
+			for lp in lowPins:
+				self.writePin(lp, GPIO.LOW)
+		if (highPins):
+			for hp in highPins:
+				self.writePin(hp, GPIO.HIGH)
 
 	def checkPin(self, pinID, expectedValue):
 		GPIO.setup(pinID, GPIO.IN)
