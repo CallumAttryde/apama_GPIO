e690cdca9aa82d98e73f2be2887e18e26a76a312
CallumAttryde <CallumAttryde@gmail.com>
2018-03-12 15:30:41 +0000

    modified ButtonPress test to be verified

--- tutorials/ButtonPress/run.py
+++ tutorials/ButtonPress/run.py
@@ -5,7 +5,9 @@ class PySysTest(GPIOBaseTest):
 	def execute(self):
 		self.start()
 		self.correlator.injectMonitorscript(filenames=['tutorial.mon'])		
-		self.wait(30)
+		self.waitForSignal('correlator.out', expr='.*Monitor Loaded.*')
+		self.writePin(22, 1)
+		self.waitForSignal('correlator.out', expr='Button Pressed')
 
 	def validate(self):
 		pass
--- tutorials/test/GPIO/BaseTest.py
+++ tutorials/test/GPIO/BaseTest.py
@@ -10,8 +10,8 @@ class GPIOBaseTest(BaseTest):
 		
 		self.BASE_DIR = os.path.join(self.project.root, '..')
 
-                GPIO.setmode(GPIO.BCM)
-                GPIO.setwarnings(False)
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setwarnings(False)
 
 		os.environ["LD_LIBRARY_PATH"] = self.BASE_DIR + os.pathsep + os.environ["LD_LIBRARY_PATH"]
 
@@ -20,10 +20,10 @@ class GPIOBaseTest(BaseTest):
 		self.correlator.start()
 		self.correlator.injectMonitorscript(filenames=[os.path.join(self.BASE_DIR, 'GPIOPlugin.mon')])
 
-        def checkPin(self, pinID, expectedValue):
-                GPIO.setup(pinID, GPIO.IN)
-                self.assertTrue(expectedValue == GPIO.input(pinID))
+	def checkPin(self, pinID, expectedValue):
+		GPIO.setup(pinID, GPIO.IN)
+		self.assertTrue(expectedValue == GPIO.input(pinID))
 
-        def writePin(self, pinID, value):
-                GPIO.setup(pinID, GPIO.OUT)
-                GPIO.output(pinID, value)
+	def writePin(self, pinID, value):
+		GPIO.setup(pinID, GPIO.OUT)
+		GPIO.output(pinID, value)
