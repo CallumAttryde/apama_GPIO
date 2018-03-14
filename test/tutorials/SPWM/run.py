b137ee524e1d9a20ec43cb1c7577e9810b6f8660
sharon <golden_sharon@yahoo.co.uk>
2018-03-14 15:46:16 +0000

    Updated tutorial for SPWM

--- test/tutorials/SPWM/run.py
+++ test/tutorials/SPWM/run.py
@@ -4,11 +4,9 @@ class PySysTest(GPIOBaseTest):
 
 	def execute(self):
 		self.start()
-		self.initialPinSetup(lowPins=[17])
 
 		self.correlator.injectMonitorscript(filenames=['tutorial.mon'])
 		self.waitForSignal('correlator.out', expr='LED off', errorExpr=['TEST FAILED'], timeout=10)
-		self.checkPin(17, 0)
 		print "LED OFF"
 		self.correlator.sendEventStrings('ActivateSoftPWM(0, 100)')
 		self.waitForSignal('correlator.out', expr='Test Complete', errorExpr=['Error'], timeout=60)
