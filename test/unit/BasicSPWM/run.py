5fd0ab4016ee6329410061f97e9ae23437f9a72d
sharon <golden_sharon@yahoo.co.uk>
2018-03-15 16:20:55 +0000

    tidy up

--- test/unit/BasicSPWM/run.py
+++ test/unit/BasicSPWM/run.py
@@ -8,7 +8,7 @@ class PySysTest(GPIOBaseTest):
 		self.correlator.injectMonitorscript(filenames=['tutorial.mon'])
 		self.waitForSignal('correlator.out', expr='LED off', errorExpr=['TEST FAILED'], timeout=10)
 		self.correlator.sendEventStrings('ActivateSoftPWM({500:1500,501:5000,790:800})')
-		#33.33% 10% and 98.75%
+		# Expected results ~ 33.33% 10% and 98.75%
 		self.waitForSignal('correlator.out', expr='Test Complete', errorExpr=['Error','Test Failed'], timeout=60)
 
 	def validate(self):
